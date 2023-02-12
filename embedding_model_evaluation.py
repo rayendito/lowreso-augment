6e5f02d08abcab708ccfeca01044873eef494f6e
rayendito <13519157@std.stei.itb.ac.id>
2023-02-12 10:25:59 +0700

    runnnig exps

--- embedding_model_evaluation.py
+++ embedding_model_evaluation.py
@@ -1,8 +1,10 @@
 import sys        
 sys.path.append('./src')
-from embd_evaluator.embeddingsp_eval import evaluate_compactness_score
+
 import fasttext
 import pickle
+from embd_evaluator.embeddingsp_eval import evaluate_compactness_score
+from augmentators.augment_embeddingsp import train_fasttext_model
 
 
 import pickle
@@ -11,8 +13,17 @@ f = open('./data/augment_embeddingsp/embedding_model_evaluation/compactness_test
 refs = list(pickle.load(f).values())
 f.close()
 
-model = fasttext.load_model('./data/augment_embeddingsp/embedding_models/jv.cbow.20.300.bin')
-compactness_scores = evaluate_compactness_score(model, refs)
 
-print("===========================COMPACTNESS SCORE===========================")
-print(compactness_scores)
+
+print("===========================COMPACTNESS SCORES===========================")
+method = "skipgram"
+for epoch in range(20):
+    model_path = train_fasttext_model('./data/augment_embeddingsp/javanese_mono/java_mono_clean',
+                                        method = method,
+                                        minn = 2,
+                                        maxn=4,
+                                        epoch = epoch,
+                                        lr = 0.05)
+    model = fasttext.load_model('./data/augment_embeddingsp/embedding_models/jv.{}.{}.300.bin'.format(method, epoch))
+    compactness_scores = evaluate_compactness_score(model, refs)
+    print("method {} epoch {} : compactness {}".format(method, epoch, compactness_scores))
