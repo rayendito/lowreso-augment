9ee2c6f880463654632d219756115a87d42f901c
rayendito <13519157@std.stei.itb.ac.id>
2023-02-28 10:49:58 +0700

    enabled CUDA

--- src/augmentators/augmentator_insertion.py
+++ src/augmentators/augmentator_insertion.py
@@ -6,29 +6,31 @@ from torch.linalg import norm
 from loaders.load_model_and_tokenizer import load_model_and_tokenizer
 from sentence_splitter import SentenceSplitter, split_text_into_sentences
 
-def _get_torch_tensor_sim(a, b):
-  return torch.dot(a/norm(a), b/norm(b)).item()
-
-def _get_BERT_sentence_representation(sentence, model, tokenizer):
-  encoded_input = tokenizer(sentence, return_tensors='pt')
+def _get_BERT_sentence_representation(sentence, model, tokenizer, device):
+  encoded_input = tokenizer(sentence, return_tensors='pt').to(device)
   output = model(**encoded_input)
+  encoded_input = encoded_input.to("cpu")
   return output['last_hidden_state'][0][0]
 
 def _get_representation_dict(corpus, model, tokenizer):
   dic = {}
+  device = "cuda:0" if torch.cuda.is_available() else "cpu"
   for sentence in corpus:
-    dic[sentence] = _get_BERT_sentence_representation(sentence, model, tokenizer)
+    dic[sentence] = _get_BERT_sentence_representation(sentence, model, tokenizer, device)
   return dic
   
+def _get_torch_tensor_sim(a, b):
+  return torch.dot(a/norm(a), b/norm(b)).item()
+
 def _get_closest_from_a_sentence(sentence, model, tokenizer, representation_dict):
   sent_rep = _get_BERT_sentence_representation(sentence, model, tokenizer)
   closest_sentence= ''
   highest_similarity = 0
 
   for sent in representation_dict:
-    dist = _get_torch_tensor_sim(sent_rep, representation_dict[sent])
-    if(dist >= highest_similarity):
-      highest_similarity = dist
+    score = _get_torch_tensor_sim(sent_rep, representation_dict[sent])
+    if(score >= highest_similarity):
+      highest_similarity = score
       closest_sentence = sent
     
   return closest_sentence
@@ -49,13 +51,10 @@ def augment_parallel_with_insertion(target_corpus, mono_corpus, dataset_name, sr
   print("Getting vector representation for sentences...")
   representation_dict = _get_representation_dict(mono_corpus, model, tokenizer)
 
-  for thing in representation_dict:
-    print(thing)
-    print(representation_dict[thing])
-    break
-  # for sent in tqdm(target_corpus):
-  #   closest_sentence = _get_closest_from_a_sentence(sent, model, tokenizer, representation_dict)
-  #   new_tgt_sentence = _insert_a_sentence_to_a_sentence(splitter, sent, closest_sentence)
+  for sent in tqdm(target_corpus):
+    closest_sentence = _get_closest_from_a_sentence(sent, model, tokenizer, representation_dict)
+    new_tgt_sentence = _insert_a_sentence_to_a_sentence(splitter, sent, closest_sentence)
+    print(new_tgt_sentence)
   #   new_src_sentence = _translate_a_sentence()
 
   #   df_augmented = pd.DataFrame([(new_src_sentence, new_tgt_sentence)], columns =[src_lang, tgt_lang])
--- src/loaders/load_model_and_tokenizer.py
+++ src/loaders/load_model_and_tokenizer.py
@@ -1,13 +1,23 @@
+import torch
 from transformers import    MBartForConditionalGeneration, \
                             MBart50TokenizerFast, \
                             AutoModel, \
                             AutoTokenizer \
 
 def _load_model(model_path):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
     if (model_path == 'facebook/mbart-large-50'):
-        return MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50")
+        model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50")
     else:
-        return AutoModel.from_pretrained(model_path)
+        model = AutoModel.from_pretrained(model_path)
+    
+    if(device == 'cuda:0'):
+        print("CUDA available, sending model to GPU")
+    else:
+        print("CUDA not available, model in CPU")
+    
+    return model.to(device)
 
 def _load_tokenizer(model_path):
     if (model_path == 'facebook/mbart-large-50'):
